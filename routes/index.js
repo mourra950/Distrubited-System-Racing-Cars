d758c2df8b271369c33f06de36876f98c301356c
mourra950 <mourra24@gmail.com>
2023-05-21 20:14:11 +0300

    Learning and testing node api functions
    learning how to create middleware by reading the official documentation from nodejs express site about the middleware

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.body)
+  res.send('Hello ' + req.body.name);
 });
+router.put('/', function(req, res, next) {
+  res.status(404).json({put:{test:'hassan'}});
+});
+
 
 
 module.exports = router;
