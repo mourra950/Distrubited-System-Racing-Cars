a65071eaa3d8faaad4a5b4df110b408bf08a6fb1
OmarElbanna <elbanna16@gmail.com>
2023-05-28 19:37:25 +0300

    Implemented simple server chat module

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
-  
-  socket.on("my message", (...args) => {
-    console.log("Emitted")
+  console.log("a user connected");
+  socket.emit('test',{'msg':"I'm here"})
+  socket.on("my message", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit('br',msg);
   });
-
-
-
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
+
 
 
 httpServer.listen(3000);
