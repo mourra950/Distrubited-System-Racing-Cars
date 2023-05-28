f6ff47ac5285df1bbc6815fce9d755c89a07c083
mourra950 <mourra24@gmail.com>
2023-05-28 20:56:52 +0300

    added a way to read messages

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
-app = QApplication(sys.argv)
 import socketio
 import keyboard
 from datetime import datetime
+
+chat_list = []
+app = QApplication(sys.argv)
 sio = socketio.Client()
+
+
 @sio.on('connect')
 def connect_handler():
     print('Connected!')
 
+
 @sio.event
 def br(data):
-    print(data['msg'])
+    current_time = datetime.utcnow().strftime("%H:%M:%S:%f")[:-3]
+    chat_list.append((str(data['msg']),str(current_time)))
+
 
 @sio.event
 def test(data):
     print(data['msg'])
 
-    
+
 class Home(QMainWindow):
     def __init__(self) -> None:
         """
@@ -31,29 +38,40 @@ class Home(QMainWindow):
         uic.loadUi('./client.ui', self)
         # searching for the buttons by ID and referencing them
         self.send = self.findChild(QPushButton, 'send_btn')
+        self.chat = self.findChild(QTextEdit, 'chat_txt')
         self.msg = self.findChild(QTextEdit, 'message_txt')
+        self.re = self.findChild(QPushButton, 'refresh')
 
         # assign event listeners when the button is clicked to excute a function
         self.send.clicked.connect(self.send_msg)
-    
+        self.re.clicked.connect(self.inserting)
+
     def send_msg(self):
         value = self.msg.toPlainText()
         self.emitting(value)
 
-    def emitting(self,value):
-        now = datetime.now()
-        sio.emit('my message',{'msg': value})
-        current_time = now.strftime("%H:%M:%S")
-        print("Current Time =", current_time)
+    def inserting(self):
+        self.chat.clear()
+        for i in chat_list:
+            self.chat.insertPlainText(str(i))
+            self.chat.insertPlainText('\n')
+            
+
+    def emitting(self, value):
+        current_time = datetime.utcnow().strftime("%H:%M:%S:%f")[:-3]
+        chat_list.append((str(value),str(current_time)))
+        sio.emit('my message', {'msg': value})
+        self.inserting()
+        # print("Current Time =", current_time)
+
 
 def run():
-    sio.connect('http://localhost:3000')
+    sio.connect('https://race-car.onrender.com')
     home = Home()
     home.show()
     app.exec_()
 
-if __name__ == '__main__':
-    
-    run()
 
-    
+if __name__ == '__main__':
+
+    run()
