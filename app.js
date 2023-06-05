7235b935d3dcf2bbf7c85b67f6f2fbef4bec27eb
mourra950 <mourra24@gmail.com>
2023-06-05 15:34:54 +0300

    updating the server
    starting to code the game and communication logic with  session and lobby capabilities

--- app.js
+++ app.js
@@ -7,14 +7,37 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
 io.on("connection", (socket) => {
+
   console.log("a user connected");
   socket.on("my message", (msg) => {
     console.log(msg);
-    socket.broadcast.emit('br',msg);
+    socket.broadcast.emit('br', msg);
+  });
+  socket.on("testunity", (msg) => {
+    console.log(msg);
   });
 
-  socket.on("joinRoom",(msg)=>{
-    
+  socket.on("CreateRoom", () => {
+    console.log(socket.id)
+    socket.join(socket.id)
+    socket.emit('createRoomStatus', { ID: socket.id })
+  })
+
+  socket.on("joinRoom", (data) => {
+    const rooms = io.of("/").adapter.rooms;
+
+    console.log(rooms.has(data.roomID))
+    console.log(rooms.get(data.roomID).size)
+
+    if (rooms.has(data.roomID)) {
+      socket.join(data.roomID)
+      console.log(rooms)
+      socket.emit('roomStatus', { status: 'true' })
+
+    }
+    else {
+      socket.emit('roomStatus', { status: 'false' })
+    }
     //socket.join();
   })
 
