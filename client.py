88179b50074afe251f43299c48ec153592d8323a
OmarElbanna <elbanna16@gmail.com>
2023-06-11 11:58:26 +0300

    Updated create room functionality
    sending room name from unity to python client
    
    Co-Authored-By: Omar Mohamed Yousef <64339763+mourra950@users.noreply.github.com>

--- client.py
+++ client.py
@@ -77,7 +77,7 @@ def unityReceive():
                             x, z = data.split(',')
                             sio.emit('testunity', {'x': x, 'z': z})
                         elif func == '/Create':
-                            sio.emit('CreateRoom',{'RoomID':'roomOmar'})
+                            sio.emit('CreateRoom',{'RoomID':data})
                         elif func == '/Join':
                             print(data)
                             sio.emit('joinRoom',{'RoomID':data})
