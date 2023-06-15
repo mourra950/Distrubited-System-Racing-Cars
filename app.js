b8f20b9ae37105a0b60f897434b4d0d7ee674d4b
mourra950 <mourra24@gmail.com>
2023-06-15 23:24:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,23 +5,6 @@ const { Server } = require("socket.io");
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
-const MongoClient = require('mongodb').MongoClient
-
-const uri = "mongodb+srv://omarmayousef:G7IQyLiT1OKcn0Lj@cluster0.shg8nan.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const DBClient = new MongoClient(uri);
-DBClient.connect()
-console.log("DB connected");
-const db = DBClient.db('uuid_db');
-const uuid_collection = db.collection('uuid_coll');
-const room_collection = db.collection('room_coll');
-
-//collection.findOne({'uuid':uuid});
-
-
-
-
-
 
 io.on("connection", (socket) => {
 
@@ -39,16 +22,6 @@ io.on("connection", (socket) => {
 
     if (!(rooms.has(data.RoomID))) {
       socket.join(data.RoomID)
-      const doc = {
-        'RoomID':data.RoomID,
-        "UserID": data.UserID
-      }
-      room_collection.insertOne(doc)
-    const query = { "RoomID": "omar4" };
-    res = room_collection.find(query)
-    for  (const doc of res) {
-      console.log(doc);
-    }
       socket.emit('createRoomStatus', { 'status': 'true', 'UserID': socket.id, 'RoomID': data.RoomID })
     }
     else {
@@ -58,16 +31,12 @@ io.on("connection", (socket) => {
 
   socket.on("joinRoom", (data) => {
     const rooms = io.of("/").adapter.rooms;
-    console.log(rooms)
 
+
+    console.log(rooms)
 
     if (rooms.has(data.RoomID)) {
       socket.join(data.RoomID)
-      const doc = {
-        'RoomID':data.RoomID,
-        "UserID": data.UserID
-      }
-      room_collection.insertOne(doc)
       socket.emit('roomStatus', { 'status': 'true', 'RoomID': data.RoomID, 'UserID': socket.id })
     }
     else {
@@ -78,11 +47,6 @@ io.on("connection", (socket) => {
 
   socket.on("refreshplayers", (data) => {
     const rooms = io.of("/").adapter.rooms;
-    const query = { "RoomID": data.RoomID };
-    res = room_collection.find(query)
-    res.toArray(function(err,res){
-      console.log(res);
-    });
     msg = ''
     rooms.get(data.RoomID).forEach((id) => {
       msg += id + ','
@@ -103,7 +67,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("Coord", (data) => {
-    socket.to(data.RoomID).emit('CoordBroadcast', { 'UserID': data.UserID, 'msg': data.msg})
+    socket.to(data.RoomID).emit('CoordBroadcast', { 'UserID': data.UserID, 'msg': data.msg })
   })
 
 
